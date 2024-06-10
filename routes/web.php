56f8c0380ebea7854046375001be9f6ee54b1a42
Haps <thiamhapstou@gmail.com>
2024-06-10 02:02:22 +0000

    ajout des commentaires sur les routes

--- routes/web.php
+++ routes/web.php
@@ -10,17 +10,35 @@ use App\Models\Categorie;
 use App\Models\Personnel;
 use Illuminate\Support\Facades\Route;
 
+// Routes publiques
 Route::get('/', [ProduitController::class,'listeAccueil']);
 Route::get('/categorie/{categorie}', [ProduitController::class, 'produitsParCategorie'])->name('produits.par.categorie');
-Route::get('/boutique' , [ProduitController::class, 'boutique']);
+Route::get('/boutique', [ProduitController::class, 'boutique']);
 
+// Routes d'authentification du client
+Route::get('/inscriptionClient', [ClientController::class, 'ajoutClient']);
+Route::post('/sauvegardeClient', [ClientController::class , 'sauvegardeClient']);
+Route::get('/connexionClient', [ClientController::class, 'connexion']);
+Route::post('/traitementConnexionClient', [ClientController::class, 'traitementConnexion']);
+Route::get('/deconnexionClient', [ClientController::class, 'deconnexion']);
 
-Route::get('inscription', [PersonnelController::class, 'ajoutPersonnel']);
-Route::post('/sauvegardePersonnel', [PersonnelController::class , 'sauvegardePersonnel']);
-Route::get('/connexion', [PersonnelController::class, 'connexion']);
-Route::post('/traitementConnexion', [PersonnelController::class, 'traitementConnexion']);
-Route::get('/deconnexion', [PersonnelController::class, 'deconnexion']); 
+// Middleware pour les utilisateurs authentifiés en tant que client
+Route::middleware(['EnsureUserIsClient'])->group(function () {
+    Route::get('/profil', [ProduitController::class,'produitCategorie']);
+    Route::post('/ajoutPanier/{produits}', [PanierController::class, 'ajoutPanier']);
+    Route::get('/voirPanier', [PanierController::class, 'voirPanier']);
+    Route::post('/supprimerDuPanier', [PanierController::class, 'supprimerDuPanier']);
+    Route::get('/validerCommande', [PanierController::class, 'validerCommande']);
+    Route::post('/traiterCommande', [PanierController::class, 'traiterCommande']);
+    Route::get('/shop' , [ProduitController::class, 'shop']);
+    Route::get('/categories/{categorie}', [ProduitController::class, 'produitsParCategories']);
+    Route::get('/mesCommandes', [ClientController::class, 'mesCommandes'])->name('mesCommandes');
+    Route::get('/commande/{id}', [ClientController::class, 'detailCommande'])->name('detailCommande');
+    Route::get('/commande/{id}/edit', [PanierController::class, 'edit'])->name('commande.edit');
+    Route::post('/commande/{id}/update', [PanierController::class, 'update'])->name('commande.update');
+});
 
+// Routes nécessitant une session vérifiée
 Route::middleware(['CheckSession'])->group(function () {
     Route::get('/categories', [CategorieController::class, 'listeCategories']);
     Route::get('/ajoutCategorie', [CategorieController::class, 'ajoutCategorie']);
@@ -36,32 +54,13 @@ Route::middleware(['CheckSession'])->group(function () {
     Route::get('/modificationProduit/{id}', [ProduitController::class, 'modificationProduit']);
     Route::post('/modificationProduit', [ProduitController::class, 'sauvegardeModification']);
 
-
-Route::get('/touteLesCommandes', [PersonnelController::class, 'toutesLesCommandes']);
-Route::get('/commandePersonnel/{id}', [PersonnelController::class, 'detailCommande']);
+    Route::get('/touteLesCommandes', [PersonnelController::class, 'toutesLesCommandes']);
+    Route::get('/commandePersonnel/{id}', [PersonnelController::class, 'detailCommande']);
 });
-Route::get('/inscriptionClient', [ClientController::class, 'ajoutClient']);
-Route::post('/sauvegardeClient', [ClientController::class , 'sauvegardeClient']);
-Route::get('/connexionClient', [ClientController::class, 'connexion']);
-Route::post('/traitementConnexionClient', [ClientController::class, 'traitementConnexion']);
-Route::get('/deconnexionClient', [ClientController::class, 'deconnexion']);
 
-Route::middleware(['EnsureUserIsClient'])->group(function () {
-Route::get('/profil', [ProduitController::class,'produitCategorie']);
-Route::post('/ajoutPanier/{produits}', [PanierController::class, 'ajoutPanier']);
-Route::get('/voirPanier', [PanierController::class, 'voirPanier']);
-Route::post('/supprimerDuPanier', [PanierController::class, 'supprimerDuPanier']);
-Route::get('/validerCommande', [PanierController::class, 'validerCommande']);
-Route::post('/traiterCommande', [PanierController::class, 'traiterCommande']);
-Route::get('/shop' , [ProduitController::class, 'shop']);
-Route::get('/categories/{categorie}', [ProduitController::class, 'produitsParCategories']);
-Route::get('/mesCommandes', [ClientController::class, 'mesCommandes'])->name('mesCommandes');
-Route::get('/commande/{id}', [ClientController::class, 'detailCommande'])->name('detailCommande');
-Route::get('/commande/{id}/edit', [PanierController::class, 'edit'])->name('commande.edit');
-Route::post('/commande/{id}/update', [PanierController::class, 'update'])->name('commande.update');
-
-
-    });
-
-
- 
+// Routes d'authentification du personnel
+Route::get('inscription', [PersonnelController::class, 'ajoutPersonnel']);
+Route::post('/sauvegardePersonnel', [PersonnelController::class , 'sauvegardePersonnel']);
+Route::get('/connexion', [PersonnelController::class, 'connexion']);
+Route::post('/traitementConnexion', [PersonnelController::class, 'traitementConnexion']);
+Route::get('/deconnexion', [PersonnelController::class, 'deconnexion']);
